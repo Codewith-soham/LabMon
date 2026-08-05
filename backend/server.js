d957b8fc8e8891d0fed66b4a6e2f415b3f3f25c4
Codewith-soham <ghadgesoham2006@gmail.com>
2026-08-05 19:33:26 +0530

    Setup of project and connecting database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
-import express from "express"
-
-const app = express()
-
-const port = 8000
-
-app.listen(port, () => {
-    console.log(`Server running on ${port}`)
+import dotenv from "dotenv"
+dotenv.config({
+   path: ".env" 
 })
+import {app} from "./src/app.js"
+import connectDB from "./src/config/db.config.js"
+
+const PORT = process.env.PORT
+
+connectDB()
+    .then(() => {
+        const server = app.listen(PORT, () => {
+            console.log(`Server connected to ${PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.error("Failed to connect database")
+    })

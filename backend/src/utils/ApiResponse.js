2c7e80217c86cb9f62fbc515c8e8a8c306ddd05f
Codewith-soham <ghadgesoham2006@gmail.com>
2026-08-07 22:10:47 +0530

    feat: created ApiResponse util for constant api response

---
+++ backend/src/utils/ApiResponse.js
@@ -0,0 +1,12 @@
+//used to give consistent api responses
+
+class ApiResponse {
+  constructor(statusCode, data, message = "Success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400; //400 above gives error
+  }
+}
+
+export { ApiResponse };

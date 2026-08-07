6a31c376f6258819f877c500fdb070ba76a932d2
Codewith-soham <ghadgesoham2006@gmail.com>
2026-08-07 22:36:23 +0530

    feat: modified constants.js

--- backend/src/config/constants.js
+++ backend/src/config/constants.js
@@ -1,6 +1,16 @@
+//Centralizing roles and complaint_status so that we can change role if we want without updating in different files
+
 export const ROLES = {
     ADMIN: "admin",
     LAB_INCHARGE: "labIncharge",
     HOD: "hod",
     DEAN_INFRA: "deanInfra"
+}
+
+export const COMPLAINT_STATUS = {
+    OPEN: "Open",
+    ESCALATED_HOD: "Escalated_HOD",
+    ESCALATED_DEAN: "Escalated_Dean",
+    RESOLVED: "Resolved"
+
 }
